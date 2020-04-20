7caa07e8f33f54c459b1545d07b1e90f0481902c
Anna_Kiniova <anna.kiniova@gmail.com>
2020-04-20 12:45:12 +0300

    beautify: add mock data for tests

--- src/common/db.start.js
+++ src/common/db.start.js
@@ -2,43 +2,64 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const { MONGO_CONNECTION_STRING } = require(path.join(__dirname, './config'));
-const User = require(path.join(__dirname, '../resources/users/user.model'));
-const Board = require(path.join(__dirname, '../resources/board/board.model'));
+// mock data to test
 
-const users = [
-  new User({ name: 'user1', login: 'login1', password: 'password1' }),
-  new User({ name: 'user2', login: 'login2', password: 'password2' })
-];
+// const User = require(path.join(__dirname, '../resources/users/user.model'));
+// const Board = require(path.join(__dirname, '../resources/board/board.model'));
+// const Task = require(path.join(__dirname, '../resources/task/task.model'));
 
-const boards = [
-  new Board({
-    title: 'board1',
-    columns: [
-      {
-        title: 'test 11',
-        order: 1
-      },
-      {
-        title: 'test 12',
-        order: 2
-      }
-    ]
-  }),
-  new Board({
-    title: 'board2',
-    columns: [
-      {
-        title: 'test 21',
-        order: 1
-      },
-      {
-        title: 'test 22',
-        order: 2
-      }
-    ]
-  })
-];
+// const users = [
+//   new User({ name: 'user1', login: 'login1', password: 'password1' }),
+//   new User({ name: 'user2', login: 'login2', password: 'password2' })
+// ];
 
+// const boards = [
+//   new Board({
+//     id: '1',
+//     title: 'board1',
+//     columns: [
+//       {
+//         title: 'test 11',
+//         order: 1
+//       },
+//       {
+//         title: 'test 12',
+//         order: 2
+//       }
+//     ]
+//   }),
+//   new Board({
+//     title: 'board2',
+//     columns: [
+//       {
+//         title: 'test 21',
+//         order: 1
+//       },
+//       {
+//         title: 'test 22',
+//         order: 2
+//       }
+//     ]
+//   })
+// ];
+// const tasks = [
+//   new Task({
+//     title: 'test task 1',
+//     order: 1,
+//     description: 'Lorem ipsum',
+//     boardId: '1',
+//     userId: null,
+//     columnId: null
+//   }),
+//   new Task({
+//     title: 'test task 2',
+//     order: 2,
+//     description: 'Lorem ipsum',
+//     boardId: '1',
+//     userId: null,
+//     columnId: null
+//   })
+// ];
 const connectDB = callback => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
     useNewUrlParser: true,
@@ -49,8 +70,11 @@ const connectDB = callback => {
   db.once('open', async () => {
     console.log('db is connected');
     await db.dropDatabase();
-    users.forEach(user => user.save());
-    boards.forEach(board => board.save());
+    // mock data to test
+
+    // users.forEach(user => user.save());
+    // boards.forEach(board => board.save());
+    // tasks.forEach(task => task.save());
     callback();
   });
 };
